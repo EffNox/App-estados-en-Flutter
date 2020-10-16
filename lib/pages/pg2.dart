5ca1fcc5bd4c354a13e11d3e90045bce7fedcad7
NixRoYal <fernando_1999_ticona_@hotmail.com>
2020-10-16 14:04:34 -0500

    Fin GetX - Gestor de estado de flutter

--- lib/pages/pg2.dart
+++ lib/pages/pg2.dart
@@ -1,6 +1,11 @@
+import 'package:estados/controllers/CUsuario.dart';
+import 'package:estados/models/usuario.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class Pg2 extends StatelessWidget {
+  // final clUsu = Get.find<CUsuario>();
+  final CUsuario clUsu = Get.find();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,19 +16,27 @@ class Pg2 extends StatelessWidget {
           children: [
             MaterialButton(
                 child: Text('Establecer Usuario'),
-                onPressed: () {},
+                onPressed: () {
+                  final u =
+                      Usuario(nombre: 'Fernando', edad: 21, profesiones: []);
+                  clUsu.updateUser(u);
+                },
                 color: Colors.blue),
             MaterialButton(
                 child: Text('Cambiar Edad'),
-                onPressed: () {},
+                onPressed: () => clUsu.updateEdad(30),
                 color: Colors.blue),
             MaterialButton(
                 child: Text('Añadir Profesion'),
-                onPressed: () {},
+                onPressed: clUsu.addProfesion,
                 color: Colors.blue),
           ],
         ),
       ),
+      // floatingActionButton: FloatingActionButton(
+      // child: Icon(Icons.accessibility_new),
+      // onPressed: () => Get.offNamed('pg1'),
+      // ),
     );
   }
 }
